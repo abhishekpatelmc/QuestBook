30142f6368c5e70e47574662d1641b9b33b229b9
Abhishek patel <mcabhishekpatel2017@gmail.com>
2023-03-22 02:47:41 -0400

    Fix: #1 added model.py

--- home/models.py
+++ home/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images')
+
+    class Meta:
+        verbose_name_plural = 'Countries'
+    
+
+    def __str__(self):
+        return self.name
+    
+
+class Adventure(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images')
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
