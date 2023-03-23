a926f8a3496e1b589355ec0eed2831fa77f16f23
Hetvi2799 <hetvikhirsaria1999@gmail.com>
2023-03-23 01:49:28 -0400

    styling to index page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     return render(request, 'home/index.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.info(request, 'Sucessfully Logged in')
+            email = request.user.email
+            print(email)
+            content = 'Hello ' + request.user.first_name + ' ' + request.user.last_name + '\n' + 'You are logged in in our site.keep connected and keep travelling.'
+            # send_mail('Alert for Login', content
+            #           , 'travellotours89@gmail.com', [email], fail_silently=True)
+            # dests = Destination.objects.all()
+            # return render(request, 'index.html',{'dests':dests})
+        else:
+            messages.info(request, 'Invalid credential')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
