58822d25fcb20973c1d64e6a812511f330479730
Abhishek patel <mcabhishekpatel2017@gmail.com>
2023-03-23 21:28:21 -0400

    Fix: Changed model.py

--- home/models.py
+++ home/models.py
@@ -1,26 +1,35 @@
 from django.db import models
 
-
 # Create your models here.
-class Country(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    image = models.ImageField(upload_to='images')
+class Destination(models.Model):
+    id = models.AutoField(primary_key=True)
+    country = models.CharField(max_length=20)
+    description = models.TextField(max_length=250)
+    img1 = models.ImageField(upload_to='images',blank=True)
+    img2 = models.ImageField(upload_to='images',blank=True)    
+    number = models.IntegerField(default=2)
 
     class Meta:
         verbose_name_plural = 'Countries'
     
 
     def __str__(self):
-        return self.name
+        return self.country
     
 
-class Adventure(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    image = models.ImageField(upload_to='images')
-    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+class Detailed_desc(models.Model):
+    dest_id = models.AutoField(primary_key=True)
+    country = models.CharField(max_length=20)
+    price = models.IntegerField(default=20000)
+    rating = models.IntegerField(default=5)
+    dest_name = models.CharField(max_length=25)
+    img1= models.ImageField(upload_to='images',blank=True )
+    img2 = models.ImageField(upload_to='images',blank=True )
+    desc = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'Adventures'
 
     def __str__(self):
-        return self.name
-
+        return self.dest_name
+    
--- home/urls.py
+++ home/urls.py
@@ -1,12 +1,8 @@
-from django.urls import path, include
-
+from django.urls import path
 from . import views
 
 app_name = 'home'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
-    path('register/', views.register, name='register'),
-
 ]
