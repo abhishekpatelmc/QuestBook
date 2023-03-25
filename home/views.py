f523ff434f4807aada9f7f673391a3e564078045
Abhishek patel <mcabhishekpatel2017@gmail.com>
2023-03-25 16:58:28 -0400

    Fix: Added country view and urls

--- home/views.py
+++ home/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from home.models import Destination
+from home.models import Destination, Detailed_desc
 
 def index(request):
     dests = Destination.objects.all()
     return render(request, 'home/index.html',{'dests': dests,})
+
+def destination_list(request,city_name):
+    dests = Detailed_desc.objects.all().filter(country=city_name)
+    return render(request,'home/country_details.html',{'dests': dests})
+
+def destination_details(request,city_name):
+    dest = Detailed_desc.objects.get(dest_name=city_name)
+    price = dest.price
+    request.session['price'] = price
+    request.session['city'] = city_name
+    return render(request,'home/adventure_details.html',{'dest':dest})
