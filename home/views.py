a3fe9e75ec5898626eba0fd50fabf04c4bcd03f4
Abhishek patel <mcabhishekpatel2017@gmail.com>
2023-03-25 17:31:41 -0400

    Fix: view and URL

--- home/views.py
+++ home/views.py
@@ -5,11 +5,11 @@ def index(request):
     dests = Destination.objects.all()
     return render(request, 'home/index.html',{'dests': dests,})
 
-def destination_list(request,city_name):
+def country_details(request,city_name):
     dests = Detailed_desc.objects.all().filter(country=city_name)
     return render(request,'home/country_details.html',{'dests': dests})
 
-def destination_details(request,city_name):
+def adventure_details(request,city_name):
     dest = Detailed_desc.objects.get(dest_name=city_name)
     price = dest.price
     request.session['price'] = price
