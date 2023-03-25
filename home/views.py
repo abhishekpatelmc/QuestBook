caf715532e89f4548a1bdc51812c4bb4546a3028
Abhishek patel <mcabhishekpatel2017@gmail.com>
2023-03-25 02:28:39 -0400

    Fix: Index destination display

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
+from home.models import Destination
 
 def index(request):
-    # dests = Destination.objects.all()
-    return render(request, 'home/index.html', )
-    # return render(request, 'home/index.html',{'dests': dests})
+    dests = Destination.objects.all()
+    return render(request, 'home/index.html',{'dests': dests,})
