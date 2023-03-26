9dcf9421886dcbb1e5db0c32cc1e1f80db9819bf
Pranjali <105532022+Pranjali31@users.noreply.github.com>
2023-03-26 18:21:07 -0400

    Created logout

--- home/views.py
+++ home/views.py
@@ -41,6 +41,10 @@ def login(request):
     else:
         return render(request, 'home/login.html')
 
+def logout(request):
+    auth.logout(request)
+    return redirect('index')
+
 def register(request):
     if request.method == 'POST':
         first_name = request.POST['first_name']
