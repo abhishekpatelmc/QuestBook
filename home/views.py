e4ea3b45e03477e5499da5957e696a36abc5bbeb
Abhishek patel <mcabhishekpatel2017@gmail.com>
2023-03-25 20:43:19 -0400

    Fix: All the navigation working -- ecpet booking form added about page

--- home/views.py
+++ home/views.py
@@ -5,14 +5,16 @@ def index(request):
     dests = Destination.objects.all()
     return render(request, 'home/index.html',{'dests': dests,})
 
+def about(request):
+    return render(request, 'home/about.html')
+
 def country_details(request,city_name):
     country = Destination.objects.all()
     dests = Detailed_desc.objects.all().filter(country=city_name)
     return render(request,'home/country_details.html',{'dests': dests, 'country':country})
 
-def adventure_details(request,city_name):
-    dest = Detailed_desc.objects.get(dest_name=city_name)
+def adventure_details(request,dest_name):
+    country = Destination.objects.all()
+    dest = Detailed_desc.objects.get(dest_name=dest_name)
     price = dest.price
-    request.session['price'] = price
-    request.session['city'] = city_name
-    return render(request,'home/adventure_details.html',{'dest':dest}, {'price':price})
+    return render(request,'home/adventure_details.html',{'dest': dest, 'country':country, 'price':price})
